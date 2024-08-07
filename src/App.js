5db4acc4114ee8e77b1bbcdeb734c9ea4535f9c8
Salomon Laleye <salomonlaleye@gmail.com>
2024-08-07 19:25:00 +0200

    Add static quizz

--- src/App.js
+++ src/App.js
@@ -1,18 +1,97 @@
 import React from "react";
 import styles from "./App.module.css";
 
+const quizList = [
+  {
+    name: "German States",
+    questions: [
+      {
+        text: "What is the capital city of Bavaria (Bayern)",
+        position: 1,
+        answers: [
+          {
+            text: "Munich (München)",
+            isCorrect: true,
+          },
+          {
+            text: "Berlin",
+          },
+          {
+            text: "Frankfurt",
+          },
+        ],
+      },
+      {
+        text: "What is the capital city of Saxony (Sachsen)?",
+        position: 2,
+        answers: [
+          {
+            text: "Dresden",
+            isCorrect: true,
+          },
+          {
+            text: "Leipzig",
+          },
+          {
+            text: "Chemnitz",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "German Law",
+    questions: [
+      {
+        text: "What is the supreme law of Germany?",
+        position: 1,
+        answers: [
+          {
+            text: "Grundgesetz",
+            isCorrect: true,
+          },
+          {
+            text: "Strafgesetzbuch",
+          },
+          {
+            text: "Handelsgesetzbuch",
+          },
+        ],
+      },
+      {
+        text: "What is the highest court in Germany?",
+        position: 2,
+        answers: [
+          {
+            text: "Bundesgerichtshof",
+          },
+          {
+            text: "Bundesverfassungsgericht",
+            isCorrect: true,
+          },
+          {
+            text: "Oberlandesgericht",
+          },
+        ],
+      },
+    ],
+  },
+];
+
 const App = () => {
   return (
     <div className={styles.App}>
       <h1>Quizz App</h1>
       <p>Current Quizes</p>
-       <p> Quiz A</p>
-       <p> Quiz B</p>
-       <p> Quiz C</p>
-       <ul>
-        <li>Add New Quiz</li>
-        <li>Add New Question</li>
-       </ul>
+      <ul>
+        {quizList.map((quizz) => {
+          console.log(quizz);
+          return <li>{quizz.name}</li>;
+        })}
+      </ul>
+
+      <button>Add New Quiz</button>
+    
     </div>
   );
 };
