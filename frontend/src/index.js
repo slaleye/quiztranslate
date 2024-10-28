fcfbc836934e9bd95114eea818a2bd6c4b773bed
Salomon Laleye <salomonlaleye@gmail.com>
2024-10-28 09:47:44 +0100

    Use scss

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from  "./components/App"
-import styles from "./global.css"
+import styles from "./global.scss"
 // Render your React component instead
 const root = createRoot(document.getElementById("app"));
 root.render(<App />);
